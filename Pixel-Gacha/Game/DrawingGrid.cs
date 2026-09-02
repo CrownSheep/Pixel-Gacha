1af56c94bef3e3ea9f03e84834d1bf40eccd4ac5
CrownSheep <106444061+CrownSheep@users.noreply.github.com>
2026-09-02 16:40:08 +0300

    Add grid drawing snapping behavior

--- Pixel-Gacha/Game/DrawingGrid.cs
+++ Pixel-Gacha/Game/DrawingGrid.cs
@@ -1,9 +1,8 @@
 using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
 using MonoGame.Extended.Input;
-using Pixel_Gacha;
-using IDrawable = Pixel_Gacha.IDrawable;
 
 namespace PixelGacha;
 
@@ -11,22 +10,27 @@ public class DrawingGrid : IUpdatable, IDrawable
 {
     public MouseStateExtended mouse;
     
-    private const int GRID_WIDTH = 150;
-    private const int GRID_HEIGHT = 100;
+    private int GridWidth { get; }
+    private int GridHeight { get; }
     
-    private const int SCALE = 3;
+    private const int SCALE = 20;
+    private const int OFFSET = 80;
 
     private Texture2D gridTexture;
     private Color[] pixelData;
     
     private GraphicsDevice graphicsDevice;
+    private Color Color => Color.Red;
 
     public DrawingGrid(GraphicsDevice graphicsDevice)
     {
         mouse = MouseExtended.GetState();
         
-        gridTexture = new Texture2D(graphicsDevice, GRID_WIDTH, GRID_HEIGHT);
-        pixelData = new Color[GRID_WIDTH * GRID_HEIGHT];
+        GridWidth = graphicsDevice.Viewport.Width - OFFSET;
+        GridHeight = graphicsDevice.Viewport.Height;
+        
+        gridTexture = new Texture2D(graphicsDevice, GridWidth, GridHeight);
+        pixelData = new Color[GridWidth * GridHeight];
         
         this.graphicsDevice = graphicsDevice;
     }
@@ -36,7 +40,10 @@ public class DrawingGrid : IUpdatable, IDrawable
         mouse = MouseExtended.GetState();
         
         if (mouse.IsButtonDown(MouseButton.Left)) {
-            SetPixel(mouse.X / SCALE, mouse.Y / SCALE, Color.Black);
+            int snappedX = (int)MathF.Floor((float)mouse.X / SCALE) * SCALE;
+            int snappedY = (int)MathF.Floor((float)mouse.Y / SCALE) * SCALE;
+
+            SetPixel((snappedX - OFFSET) / SCALE, (snappedY) / SCALE, Color);
         }
 
         gridTexture.SetData(pixelData);
@@ -44,14 +51,23 @@ public class DrawingGrid : IUpdatable, IDrawable
 
     public void Draw(SpriteBatch spriteBatch, GameTime gameTime)
     {
-        spriteBatch.Draw(gridTexture, new Rectangle(0, 0, GRID_WIDTH * SCALE, GRID_HEIGHT * SCALE), Color.White);
+        if (!mouse.IsButtonDown(MouseButton.Left))
+        {
+            int snappedX = (int)MathF.Floor((float)mouse.X / SCALE) * SCALE;
+            int snappedY = (int)MathF.Floor((float)mouse.Y / SCALE) * SCALE;
+
+            spriteBatch.FillRectangle(new Rectangle(snappedX, snappedY, SCALE, SCALE), Color * 0.3f);
+        }
+
+        spriteBatch.Draw(gridTexture, new Rectangle(OFFSET, 0, GridWidth * SCALE, GridHeight * SCALE), Color.White);
+        spriteBatch.DrawRectangle(new Rectangle(OFFSET, 0, GridWidth, GridHeight), Color.Black);
     }
     
     public void SetPixel(int x, int y, Color color)
     {
-        if (x is >= 0 and < GRID_WIDTH && y is >= 0 and < GRID_HEIGHT)
+        if (x >= 0 && x < GridWidth && y >= 0 && y < GridHeight)
         {
-            pixelData[x + y * GRID_WIDTH] = color;
+            pixelData[x + y * GridWidth] = color;
         }
     }
 }
--- Pixel-Gacha/Game/IUpdatable.cs
+++ Pixel-Gacha/Game/IUpdatable.cs
@@ -1,6 +1,6 @@
 using Microsoft.Xna.Framework;
 
-namespace Pixel_Gacha;
+namespace PixelGacha;
 
 public interface IUpdatable
 {
